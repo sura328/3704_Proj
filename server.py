1b70d82143e6adecb6641857fdb79244433c16ce
Henry Patch <hjpatch27@vt.edu>
2025-12-08 21:57:19 -0500

    Revert "Season Stats webpage"

--- server.py
+++ server.py
@@ -26,10 +26,6 @@ def index():
 # --------------------------------------------
 # API ROUTES
 # --------------------------------------------
-@app.route("/seasons")
-def seasons():
-    return render_template("seasons.html")
-    
 @app.route("/players", methods=["GET"])
 def get_players():
     return jsonify([p.to_dict() for p in lb.standings()])
